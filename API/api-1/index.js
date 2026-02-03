a6c955e82d4b7c64c882b4175171c8868dc1b601
nitin kumar <nitin981275@gmail.com>
2026-02-03 21:27:11 +0530

    REST api example one

---
+++ API/api-1/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
+
+// GET REQUEST 
+app.get('/users',(req, res)=>{
+    res.status(200).json({
+        "users": ['A','B','C']
+    })
+})
+
+// POST REQUEST 
+app.post('/users',(req, res)=>{
+    res.status(201).json({
+        'message': 'data recieved',
+        data: req.body
+    })
+})
+// LISTNING PORT    
+app.listen(PORT,()=>{
+    console.log(`API server is running on port ${PORT}`)
+})
