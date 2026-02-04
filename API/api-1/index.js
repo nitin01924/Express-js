45bfe23dee3e69f5ba92531cd83e3b2d3026451f
nitin kumar <nitin981275@gmail.com>
2026-02-04 11:25:16 +0530

    Creating a API that hendels a error while user post a request but the data is empty or does't exist

--- API/api-1/index.js
+++ API/api-1/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.json());
---
+++ API/api-err-hndl/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+// HOME ROUTE
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage form api error handler");
+});
+
+// CREATE USER OR DATA
+app.post("/users", (req, res) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      error: "data is empty, failed in creation.",
+    });
+  }
+  res.status(201).json({
+    message: "data is created",
+    data: req.body,
+  });
+});
+
+// PORT LISTNING
+app.listen(3001, () => {
+  console.log("server is running on port 3001.");
+});
