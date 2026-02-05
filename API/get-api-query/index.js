bb80cfdc1cebdd450bd30eae4ccd9b8a42499855
nitin kumar <nitin981275@gmail.com>
2026-02-05 20:00:22 +0530

    Add GET /users API with query parameter filtering

--- API/get-api-query/index.js
+++ API/get-api-query/index.js
@@ -7,9 +7,22 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  const products = [{ Brand: "MSI", Price: 98000 },{Brand: "Macbook M2", Price: 70000}]
-  console.log(req.query)
-  res.send()
+  const products = [
+    { Brand: "MSI", Price: 98000 },
+    { Brand: "Macbook M2", Price: 70000 },
+  ];
+  const item = req.query.Brand;
+  //   console.log(item);
+  const itemhave = products.find((p) => p.Brand == item);
+  if (!itemhave) {
+    return res.status(400).send({
+      error: "Something Went Wrong.",
+    });
+  }
+  {
+    console.log("Item Founded!!");
+    res.send("Match Founded!!");
+  }
 });
 
 app.listen(PORT, () => {
