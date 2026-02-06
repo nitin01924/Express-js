528a34aad4adfc6dac7d2e0771b8ff94e9bd0983
nitin kumar <nitin981275@gmail.com>
2026-02-06 13:44:13 +0530

    increase reliabilty by using some code

--- API/route-params/data-with-route-params/index.js
+++ API/route-params/data-with-route-params/index.js
@@ -14,17 +14,21 @@ app.get("/users/:id", (req, res) => {
     { id: 12, name: "Naveen", age: 18 },
     { id: 13, name: "Lokesh", age: 21 },
   ];
-  const id = req.params.id;
-  const ide = userdata.find((i) => i.id == id);
+  // here we are compairing a string to a number with (==),this is working but not a good habit.
+  //   const id = req.params.id;
+  //   const ide = userdata.find((i) => i.id == id);
+
+  // Now, changing the string to a number and then compair with (===), for a good practice.
+  const id = Number(req.params.id);
+  const ide = userdata.find((i) => i.id === id);
   if (!ide) {
+    console.log("user does not exist");
     return res.status(404).json({
       error: "user is not found.",
     });
   }
-  {
-    console.log(ide);
-    res.json(ide);
-  }
+  console.log(ide);
+  res.json(ide);
 });
 
 app.listen(PORT, () => {
